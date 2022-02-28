b6807246e0c4bdb6d72bcce1554f9083966b36bd
Toxiclikith <75122082+Toxiclikith@users.noreply.github.com>
2022-03-01 00:08:42 +0530

    Update apiConfig.js

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,5 +1,5 @@
 const apiConfig = {
-    baseUrl: 'https://api.themoviedb.org/3/',
+    baseUrl: '3c3a0e4d83714f197e351f1b476bad43',
     apiKey: 'get from themoviedb.org',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
